3c045e67997b83409c000716202e101f61cb64d4
Inas Hyatt <inas.raheema@gmail.com>
2016-08-17 16:52:01 -0700

    yelp updated

--- static/js/new_yelp.js
+++ static/js/new_yelp.js
@@ -1,26 +1,56 @@
 "use strict";
 
-// This goes at the top because it needs to be defined to get called when results are displayed
-function scheduleEventListerForRestaurantButtons(){
+function restaurantAdded(){
+  console.log("study location added YAY");
+}
+
+function addRestaurant(evt){
+  console.log("poop");
+    // var id = this.id; // this is the id on the button we clicked, which is the image's id
+    var restaurantName = $(this).data('restaurant-name');
+    var latitude = $(this).data('latitude');
+    var longitude = $(this).data('longitude');
+    var address = $(this).data('address');
+    var neighborhoods = $(this).data('neighborhoods');
+    console.log(restaurantName);
+
+    $.post("/add-restaurant", {
+                               'restaurant_name': restaurantName,
+                               'latitude': latitude,
+                               'longitude': longitude,
+                               'address': address,
+                               'event_id': getEventId,
+                               'neighborhoods': neighborhoods,
+                               },
+                               restaurantAdded);
+    function getEventId() {
+    var componenets = window.location.href.split("/")
+    var idx = componenets.length - 1
+    return componenets[idx]
+    }
+
+    $('.add-button').html("Added!");
+    
+}
  
+function scheduleEventListerForRestaurantButtons(){
   $('.add-button').click(addRestaurant);
   console.log("here");
-
 }
 
 
-// AJAX to display results of query using Yelp API response
+// ################################################################################
+
+
 function displayResults(data) {
     var text = "";
     for (var i = 0; i < data.results.length; i++){
       text = text + "<button class='restaurant-button btn' type='button'id=" + "'button" + i + "'" + 
                              "data-restaurant-name=" + '"' + data.results[i].name + '"' +
                              "data-address=" + "'" + data.results[i].address + "'" +
-                          
                              "data-neighborhoods=" + '"' + data.results[i].neighborhoods + '"' +
                              "data-latitude=" + "'" + data.results[i].latitude + "'" +
                              "data-longitude=" + "'" + data.results[i].longitude + "'" + 
-                             
                              ">" +
                       "<span>" + data.results[i].name + "</span>" + 
                     "</button>";
@@ -31,23 +61,17 @@ function displayResults(data) {
 
     $('.restaurant-button').click(function() {
       var name = $(this).data('restaurant-name');
- 
       var address = $(this).data('address');
-   
       var neighborhoods = $(this).data('neighborhoods') ? ($(this).data('neighborhoods')).split(",") : null;
       var latitude = $(this).data('latitude');
       var longitude = $(this).data('longitude');
-   
       var infobox = "<table class='table'><tr><td class='title'>" + name + "</td></tr>" + 
-                             
                              "<tr><td class='title'>Address: " + address + "</td></tr>" + 
                              "<tr><td class='title'>Neighborhood: " + ( neighborhoods ? neighborhoods.join(", ") : "None" ) + 
                              "</td></tr></table>" + 
                              "<button type='button' class='add-button btn' id='button' " + 
                              "data-restaurant-name=" + '"' + name + '"' +
-                           
                              "data-address=" + "'" + address + "'" +
-                           
                              "data-neighborhoods=" + '"' + neighborhoods + '"' +
                              "data-latitude=" + "'" + latitude + "'" +
                              "data-longitude=" + "'" + longitude + "'" 
@@ -76,6 +100,8 @@ function displayResults(data) {
     // gives access to the add restaurant event listener once items are loaded
 }
 
+
+
 function submitSearch(evt) {
     evt.preventDefault();
     var formInputs = {
@@ -84,70 +110,9 @@ function submitSearch(evt) {
     };
     $('#yelpResultsPanel').removeClass('hidden');
     $('#results').html("searching...");;
-    $.post("/search-location.json",
-           formInputs,
-           displayResults
-           );
+    $.post("/search-location.json", formInputs, displayResults);
 }
+
+
 
 $("#search").on("submit", submitSearch);
-
-// sends restaurant data to server to add restaurant to list
-function addRestaurant(evt){
-  console.log("poop");
-
-    // var id = this.id; // this is the id on the button we clicked, which is the image's id
-    var restaurantName = $(this).data('restaurant-name');
-
-    var latitude = $(this).data('latitude');
-    var longitude = $(this).data('longitude');
-
-    var address = $(this).data('address');
-  
-    var neighborhoods = $(this).data('neighborhoods');
- 
-
-    console.log(restaurantName);
-
-   
-
-    $.post("/add-restaurant", {
-                               'restaurant_name': restaurantName,
-                               
-                               'latitude': latitude,
-                               'longitude': longitude,
-                               
-                               'address': address,
-                               'event_id': getEventId,
-                    
-                               'neighborhoods': neighborhoods,
-                               },
-                               getRestaurantsFromDB);
-    function getEventId() {
-    var componenets = window.location.href.split("/")
-    var idx = componenets.length - 1
-    return componenets[idx]
-    }
-
-    $('.add-button').html("Added!");
-    
-}
-
-
-// ########################################################################
-
-function getRestaurantsFromDB(data){
- 
-  console.log("in the get restaurants from db function");
-
-
-}
-
-// #################################################################
-// vanish flash messages
-
-$(document).ready(function(){
-  setTimeout(function(){
-    $('.flash').fadeOut();
-  }, 3000);
- });
