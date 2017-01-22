1fe7ed3969aca4f68b0848718604cc17ac0a21d4
Inas Hyatt <inas.raheema@gmail.com>
2017-01-22 13:22:09 -0800

    debug set to true

--- server.py
+++ server.py
@@ -474,8 +474,8 @@ def view_event_friends():
 
 if __name__ == "__main__":
 
-	app.debug = False
-	app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+	app.debug = True
+	# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 	connect_to_db(app, os.environ.get("DATABASE_URL"))
 	# Use the DebugToolbar
 	DebugToolbarExtension(app)
